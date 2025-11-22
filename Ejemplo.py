408ec6a2ca759388b9e9451fa7383ecc4021091c
Marvin Calderon <marvin.calderon@ucsp.edu.pe>
2025-11-21 20:43:58 -0500

    Cambio 1

---
+++ Ejemplo.py
@@ -0,0 +1,20 @@
+a=10
+b=30
+c=30*10
+print('Resultado =',c)
+
+d=5
+print('Resultado =',d)
+
+e=c/d
+print('Resultado =',e)
+
+f=e-b
+print('Resultado =',f)
+
+g=e-f
+
+#Cambio de valor de f y g
+f=c/a
+
+g=c-f
